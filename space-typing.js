2e032b1296f26d72b9f9318212a09ba77cdb0529
Le Van Dat <uit@luyenthihothanh.edu.vn>
2020-01-24 09:31:52 +0700

    Create space-typing.js

---
+++ space-typing.js
@@ -0,0 +1,3 @@
+$( "#inputfield" ).keyup(function() { $("#inputfield").val( $(".highlight").text() )});
+//Dán vào console, ô input tự động điền và bạn chỉ cần nhấn Space
+//Paste it into the console, the input box fills in automatically, and you just need to press Space
